af629041d19db96a43797e9771669063f924236b
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2025-07-25 08:12:46 +0900

    [PCCE 기출문제] 9번 / 지폐 접기

---
+++ Level_1/_340199.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> wallet, vector<int> bill) {
+    int answer = 0;
+    int t = 0;
+    while(1)
+    {
+        if(bill[0] <= wallet[0] && bill[1] <= wallet[1])
+            break;
+        else if(bill[0] <= wallet[1] && bill[1] <= wallet[0])
+            break;
+        
+        answer++;
+        if(bill[0] > bill[1])
+            bill[0] /= 2;
+        else
+            bill[1] /=2;
+    }
+    
+    return answer;
+}
