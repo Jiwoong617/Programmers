e6cf2bd2e1a7bc75904080942c2464f7a0733052
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2025-08-10 17:32:13 +0900

    완전범죄

--- Level_2/_389480.cpp
+++ Level_2/_389480.cpp
@@ -28,3 +28,37 @@ int solution(vector<vector<int>> info, int n, int m) {
     
     return (ans >= n ? -1 : ans);
 }
+
+
+//이 아래는 백트래킹으로 풀어서 시간초과난 코드
+/*
+#include <string>
+#include <vector>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+int ans = INT_MAX;
+
+void dfs(vector<vector<int>>& v, int& n, int& m, int a, int b, int deep)
+{
+    if (a >= n || b >= m) return;
+    
+    if (deep == v.size())
+    {
+        if (a < n && b < m)
+            ans = min(a, ans);
+
+        return;
+    }
+
+    dfs(v, n, m, a + v[deep][0], b, deep + 1);
+    dfs(v, n, m, a, b + v[deep][1], deep + 1);
+}
+
+int solution(vector<vector<int>> info, int n, int m) {
+    dfs(info, n, m, 0, 0, 0);
+    return (ans == INT_MAX ? -1 : ans);
+}
+*/
