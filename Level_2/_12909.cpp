4f0612c55293f5f5b7bf45eb0fcd0e0cd6434757
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2025-08-21 09:12:25 +0900

    올바른 괄호

---
+++ Level_2/_12909.cpp
@@ -0,0 +1,24 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int n = 0;
+    for(char c : s)
+    {
+        if(c == '(')
+            n++;
+        else
+        {
+            if(n <= 0)
+                return false;
+            else
+                n--;
+        }
+    }
+    
+    
+    return (n != 0 ? false : true);
+}
