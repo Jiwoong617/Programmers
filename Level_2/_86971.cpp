19f3f4292b23a16e794ea973bd4d48bcb179d2f0
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2025-09-11 10:14:52 +0900

    전력망을 둘로 나누기

---
+++ Level_2/_86971.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> v;
+vector<bool> visited;
+int cnt = 0;
+
+void dfs(int idx)
+{
+    if (visited[idx])
+        return;
+
+    cnt++;
+    visited[idx] = true;
+    for (int i = 0; i < v[idx].size(); i++)
+    {
+        dfs(v[idx][i]);
+    }
+}
+
+int solution(int n, vector<vector<int>> wires) {
+    int ans = 100;
+    for (int i = 0; i < wires.size(); i++)
+    {
+        v = vector<vector<int>>(n + 1, vector<int>());
+        visited = vector<bool>(n + 1, false);
+        cnt = 0;
+        for (int j = 0; j < wires.size(); j++)
+        {
+            if (i == j) continue;
+            for (int k = 0; k < wires[j].size(); k++)
+            {
+                v[wires[j][0]].push_back(wires[j][1]);
+                v[wires[j][1]].push_back(wires[j][0]);
+            }
+        }
+
+        dfs(1);
+        int m = min(cnt, n - cnt);
+        ans = min(ans, n - m*2);
+    }
+
+    return ans;
+}
