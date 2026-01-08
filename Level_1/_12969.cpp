36a8c87a3f855580b322784cb53bc7d4e2773857
허지웅 <101644572+Jiwoong617@users.noreply.github.com>
2026-01-08 22:43:12 +0900

    직사각형 별찍기

---
+++ Level_1/_12969.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    int a, b;
+    cin>>a>>b;
+    for(int i = 0; i<b; i++)
+        cout << string(a,'*') <<endl;
+    return 0;
+}
